98b04eaf83e739283ab94ff79f45aed333a85222
tluciad <tluciad@gmail.com>
2022-02-10 20:51:48 +0000

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-	printf("%lu\n", sizeof(char));
-	printf("%lu\n", sizeof(int));
-	printf("%lu\n", sizeof(float));
-	printf("%lu", sizeof(double));
+	printf("%zu\n", sizeof(char));
+	printf("%zu\n", sizeof(int));
+	printf("%zu\n", sizeof(float));
+	printf("%zu", sizeof(double));
 	return 0;
 }
 
