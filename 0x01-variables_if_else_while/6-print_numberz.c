19fff7a7050c7f9c290f0481d66a5f7675e7b3f8
tluciad <tluciad@gmail.com>
2022-02-12 02:55:29 +0000

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - all single digits b10 from 0 without printf
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+	putchar('\n');
+
+	return (0);
+}
