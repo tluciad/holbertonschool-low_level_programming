5c9053168facaf6babc85b751d822895a4f7b4ae
tluciad <tluciad@gmail.com>
2022-02-16 04:07:07 +0000

    function

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* times_table - prints the 9 times table, starting with 0
+*/
+
+void times_table(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		_putchar(n);
+		if (n != 57)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}
