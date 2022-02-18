3566c03d7d9cdf219585475521a93908d0c29109
tluciad <tluciad@gmail.com>
2022-02-18 00:11:53 +0000

    more_functions

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+*more numbers - print numbers from 0 to 14  x10
+*Return: (void)
+*/
+void more_numbers(void)
+{
+	int i, x;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (x = 0; x <= 14; x++)
+		_putchar (x);
+	_putchar ('\n');
+	}
+}
