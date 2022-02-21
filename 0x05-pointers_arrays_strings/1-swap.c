5184e64422abd03abf7ed9fc3adb06b8ecda56ef
tluciad <tluciad@gmail.com>
2022-02-21 18:45:12 +0000

    arrays

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+*swap_int - function that swaps the values of two integers
+*@a: variable to swap value
+*@b: variable to swap value
+**/
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
