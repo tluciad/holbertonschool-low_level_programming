806e3377aa9aeaa03373f4dc80478ccac7cf69c3
tluciad <tluciad@gmail.com>
2022-02-22 17:21:14 +0000

    arrays

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,27 +1,20 @@
 #include "main.h"
 /**
 *print_rev -  prints a string, in reverse, followed by a new line
-*
-*@s: char pointer
+**@s: pointer char
 *Return: (void)
 */
 void print_rev(char *s)
 {
-	char actual_char;
 	int i = 0;
 	int j = 0;
 
 		while (s[i] != '\0')
-				i++;
-				i--;
-		while (j <= i)
 		{
-			actual_char = s[j];
-			s[j] = s[i];
-			s[i] = actual_char;
-			i--;
-			j++;
+			i = i + 1;
+			s = s + 1;
 		}
-		_putchar(j);
+		for (j = 1; j <= i; j++)
+		_putchar(s[i] - j);
 	_putchar('\n');
 }
