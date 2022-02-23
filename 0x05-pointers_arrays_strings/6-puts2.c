d524e1a0087fe2ccfea9716def7ed15c498f8040
tluciad <tluciad@gmail.com>
2022-02-23 22:07:12 +0000

    copies a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,22 +3,27 @@
 *put2 - function that prints every other character of a string,
 *starting with the first character, followed by a new line.
 *@str: char string
+*Return: void
 */
 void puts2(char *str)
 {
-	int size = 0; half = 0;
+	int size = 0; 
+	int half = 0;
 
 		while (*str != '\0')
 		{
-			size++
-			str++
+			size++;
+			str++;
 		}
-			if (size %2 != 0)
+		if (size %2 != 0)
 			size = (size - 1) / 2;
 			
-			else
-				size = size / 2;
+		else
+			size = size / 2;
 
-			while (str[half])
-			{
-				size++
+		while (str[half])
+		{
+			size++;
+		}
+		_putchar(str);
+}
