237b52dc2a9dc28646146e193785667ba27c9c04
tluciad <tluciad@gmail.com>
2022-02-24 15:56:30 +0000

    revarray

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*reverse_array - reverses the content of an array of integers.
+*@n: the number of elements to swap
+*@a: an array of integers
+*Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = 0;
+
+	n--;
+	while (i < n)
+	{	
+		j = a[i];
+		a[i] = a[n];
+		a[n] = j;
+		i++;
+		n--;
+	}
+}
