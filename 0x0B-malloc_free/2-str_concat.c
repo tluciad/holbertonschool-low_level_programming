db53fe5c263d21ffeb41c953f4657e067415621b
root <root@DESKTOP-QQAMHH6.localdomain>
2022-03-09 15:39:22 -0500

    create

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,46 +2,46 @@
 #include "main.h"
 
 /**
-* str_concat - concatenates two strings
-* @s1: first string to concatenate
-* @s2: ssecond string to concatenate
-* Return: pointer to the array (Success), NULL (Error)
-*/
+ * str_concat - concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: ssecond string to concatenate
+ * Return: pointer to the array (Success), NULL (Error)
+ */
 char *str_concat(char *s1, char *s2)
 {
-char *p;
-int i, st1, st2;
+    char *p;
+    int i, st1, st2;
 
-if (s1 == NULL)
-{
-s1 = "";
-}
-if (s2 == NULL)
-{
-s2 = "";
-}
-for (st1 = 0; s1[st1] != '\0'; st1++)
-{
-}
-for (st2 = 0; s2[st2] != '\0'; st2++)
-{
-}
-p = (char *)malloc(((st1 + st2) + 1) * sizeof(char));
+    if (s1 == NULL)
+    {
+        s1 = "";
+    }
+    if (s2 == NULL)
+    {
+        s2 = "";
+    }
+    for (st1 = 0; s1[st1] != '\0'; st1++)
+    {
+    }
+    for (st2 = 0; s2[st2] != '\0'; st2++)
+    {
+    }
+    p = (char *)malloc(((st1 + st2) + 1) * sizeof(char));
 
-if (p == NULL)
-{
-return (NULL);
-}
-else
-{
-for (i = 0; i <= st1 - 1; i++)
-{
-p[i] = s1[i];
-}
-for (i = 0; i <= st2; i++)
-{
-p[i + st1] = s2[i];
-}
-return (p);
-}
+    if (p == NULL)
+    {
+        return (NULL);
+    }
+    else
+    {
+        for (i = 0; i <= st1 - 1; i++)
+        {
+            p[i] = s1[i];
+        }
+        for (i = 0; i <= st2; i++)
+        {
+            p[i + st1] = s2[i];
+        }
+        return (p);
+    }
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -32,9 +32,8 @@ int **alloc_grid(int width, int height)
 			free(grid);
 			return (NULL);
 		}
-	}
-	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
+	}	
 	return (grid);
 }
