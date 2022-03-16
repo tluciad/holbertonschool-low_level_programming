140a7f0e85a761797c870b7379ebe85acb8d2f17
tluciad <tluciad@gmail.com>
2022-03-16 16:32:01 -0500

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * print_name - Function that prints the name
+ * @name: the name
+ * @f: function ponint
+ * Return : the name
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f == NULL || name == NULL)
+		return;
+	(*f)(name);
+}
