d0af9e1ef7fc5ea77b34c360644e0fcb4f89536d
tluciad <tluciad@gmail.com>
2022-03-19 20:20:17 -0500

    libreria

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,6 +1,20 @@
 #ifndef _VAR_
 #define _VAR_
 
+#include <stdarg.h>
+#include <stdlib.h>
+
+/**
+ * struct print - prints anything
+ * @c: arguments
+ * @t_func: pointer to the functions that prints all
+ */
+typedef struct print
+{
+	char *c;
+	void (*t_func)(va_list args);
+} t_print;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
