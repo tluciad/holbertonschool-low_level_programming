e4755d5e63134e7143cea3cb045d3a5a53aa3f1f
tluciad <tluciad@gmail.com>
2022-04-01 10:54:55 -0500

    free all and head is set to null

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,13 +9,10 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	if (head == NULL)
+	if (!head && !*head)
 		return;
 
-	if (*head == NULL)
-		return;
-		
-	if (!*head)
+	while (*head != NULL)
 	{
 		temp = (*head)->next;
 		free(*head);
