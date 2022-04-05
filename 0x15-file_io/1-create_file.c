131167935ba0ce61e0724f9d9df64b670b9aa50a
tluciad <tluciad@gmail.com>
2022-04-05 18:51:40 -0500

    create file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * create_file - a function that creates a file 
+ * @filename: file
+ * @text_content: content of the file
+ * Return: int 
+ */
+int create_file(const char *filename, char *text_content)
+{
+    int fd = 0;
+    int nletters = 0;
+    int w_out = 0;
+
+    if (!filename)
+        return (-1);
+
+    if (!text_content)
+    {
+        fd = open(filename, O_CREAT, 0600);
+        if (fd == -1)
+            return (-1);
+        return (1);
+    }
+
+    fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    if (fd == -1)
+        return (-1);
+
+    for (nletters = 0; text_content[nletters]; nletters++)
+
+        w_out = write(fd, text_content, nletters);
+
+    if (w_out == -1)
+        return (-1);
+
+    close(fd);
+    return (1);
+}
