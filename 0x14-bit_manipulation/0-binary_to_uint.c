259e9805863c45f685a1715a10c8d059ef3c5c95
tluciad <tluciad@gmail.com>
2022-04-05 09:28:15 -0500

    binary to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -6,25 +6,29 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int j = 1;
-	unsigned int i = 0;
-	int c;
-	unsigned int len;
-
-	len = strlen(b);
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	for (c = len - 1; c >= 0; c--)
+	ui = 0;
+
+	for (len = 0; b[len] != '\0'; len++)
+		;
+
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[c] != '0' && b[c] != '1')
-			return (0);
-		if (b[c] == '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
-			i += j;
+			return (0);
 		}
-		j *= 2;
+
+		if (b[len] & 1)
+		{
+			ui += base_two;
+		}
 	}
-	return (i);
+
+	return (ui);
 }
