ab7de9d0dac9bb5d7122a795073706ef5074ed76
tluciad <tluciad@gmail.com>
2022-06-06 09:23:41 -0500

    create a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,22 +2,23 @@
 /**
  * hash_table_create -  function that creates a hash table.
  * @size: size of the table
- * Return: new table
+ * Return: table
  */
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-    hash_table_t *table = malloc(sizeof(hash_table_t));
-    if (table == NULL)
-    {
-        return NULL;
-    }
-    table->size = size;
-    table->array = malloc(sizeof(table->array) * size);
-    if ((table->array) == NULL)
-    {
-        return NULL;
-    }
-    free (table);
-    return table;
+	hash_table_t *table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	table->size = size;
+	table->array = malloc(sizeof(table->array) * size);
+	if ((table->array) == NULL)
+	{
+		return (NULL);
+	}
+	free(table);
+	return (table);
 }
