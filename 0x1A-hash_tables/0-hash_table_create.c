1eaf44270bdea01c19eb1cb3c0368bfa4ae1a00f
tluciad <tluciad@gmail.com>
2022-06-06 09:31:02 -0500

    create a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,8 +7,9 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *table = malloc(sizeof(hash_table_t));
+	hash_table_t *table;
 
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
