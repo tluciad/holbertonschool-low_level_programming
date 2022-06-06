fb671d51037959fa990866f551bf87ccd586080f
tluciad <tluciad@gmail.com>
2022-06-06 09:37:42 -0500

    create a table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,18 +8,25 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
+	unsigned long int i = 0;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
-	table->size = size;
-	table->array = malloc(sizeof(table->array) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if ((table->array) == NULL)
 	{
+		free(table);
+		table = NULL;
 		return (NULL);
 	}
-	free(table);
+	while (i < size)
+	{
+		table->array[i] = NULL;
+		i++;
+	}
+	table->size = size;
 	return (table);
 }
