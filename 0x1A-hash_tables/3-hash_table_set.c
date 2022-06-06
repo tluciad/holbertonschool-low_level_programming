a0a595b4d1ba722fb8b90ba2820b7bcc143d0493
tluciad <tluciad@gmail.com>
2022-06-06 16:43:26 -0500

    function set in the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,42 +9,38 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	int i = 0;
-	hash_node_t *new, *temp;
-	char *tmp_val = NULL;
-
-	if (ht == NULL || ht->array == NULL || value == NULL)
+	hash_node_t *temp;
+	
+	if (ht == NULL || value == NULL)
 		return (0);
 
-	if (strlen(key) == 0 || key == NULL)
+	if (!*key || key == NULL)
 		return (0);
 
-	tmp_val = strdup(value);
-	if (tmp_val == NULL)
-		return (0);
 	i = key_index((const unsigned char *)key, ht->size);
-
+	
 	temp = ht->array[i];
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
 		{
 			free(temp->value);
-			temp->value = tmp_val;
 			temp->value = strdup(value);
-			free(tmp_val);
 			return (1);
 		}
 		temp = temp->next;
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	temp = malloc(sizeof(hash_node_t));
+	if (temp == NULL)
 	{
-		free(new);
+		free(temp);
 		return (0);
 	}
-	new->key = strdup(key);
-	new->value = tmp_val;
-	new->next = ht->array[i];
-	ht->array[i] = new;
+	temp->key = strdup(key);
+	temp->value = strdup(value);
+
+	if(ht->array[i] != NULL)
+		temp->next = ht->array[i];
+	ht->array[i] = temp;
 	return (1);
 }
