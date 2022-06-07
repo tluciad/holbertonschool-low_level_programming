df4004583c7c1d6a7fcdb98daa2c3ef1b11fae13
tluciad <tluciad@gmail.com>
2022-06-06 22:49:34 -0500

    delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,24 +6,24 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *temp = NULL;
+	unsigned long int i;
+	hash_node_t *temp = NULL;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        while (ht->array[i] != NULL)
-        {
-            temp = ht->array[i]->next;
-            free(ht->array[i]->key);
-            free(ht->array[i]->value);
-            free(ht->array[i]);
-            ht->array[i] = temp;
-        }
-        free(ht->array[i]);
-    }
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			temp = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = temp;
+		}
+		free(ht->array[i]);
+	}
+	free(ht->array);
+	free(ht);
 }
