f8ac649e170cc05efc8440a5b1afad77b81ea08b
tluciad <tluciad@gmail.com>
2022-08-13 17:14:56 -0500

    jump

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -15,6 +15,9 @@ int jump_search(int *array, size_t size, int value)
 	size_t end;
 	size_t i;
 
+	if (array == NULL || size == 0)
+		return(-1);
+
 	if (array != NULL && size > 0)
 	{
 		end = sqrt(size);
